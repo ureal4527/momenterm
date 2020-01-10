64953fb366bd8eaf77c25c74c3536631954b9b11
ureal4527 <ureal4527@google.com>
2020-01-10 18:02:42 +0900

    https://github.com/ureal4527/momenterm

---
+++ greeting.js
@@ -0,0 +1,42 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
+
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}    
+function handleSubmit(event){
+    event.preventDefault();    // 이벤트 작용 막는 것이 안됨 나중에 다시 확인
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+    console.log("1111");
+}    
+
+function paintGreeting(text){
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+}
+ function loadName(){
+        const currentUser = localStorage.getItem(USER_LS);
+        if(currentUser === null){
+            console.log("2222");
+            askForName();
+        }else{
+            paintGreeting(currentUser);
+        }
+    }
+
+function init(){
+ loadName();
+}
+init();
